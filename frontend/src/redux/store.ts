17042617cb5352fe1d544b30dac3680d3268b434
SAILIN HTET <sailinhtet.d@gmail.com>
2026-07-18 21:20:15 +0700

    Remove unused adapter files, update store

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,20 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import userReducer from "./features/user";
-// import socketReducer from "./features/socket/socket";
-// import topupReducer from "./features/topup";
-// import rechargeReducer from "./features/recharge";
-// import gameReducer from "./features/game";
-// import statusRechargeReducer from "./features/statusRecharge";
-// import topupAdapter from "./features/adapter/Topup";
-// import HistroyAdapter from "./features/adapter/history";
 
 import { type TypedUseSelectorHook, useSelector } from "react-redux";
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     user: userReducer,
-    // histroy: HistroyAdapter,
   },
   preloadedState: {},
   middleware: (getDefaultMiddleware) =>
